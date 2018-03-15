6df0dd67d9c5a089f80ea4527cd744e15e9e6a73
Nicolás Campuzano <ncampuzano@users.noreply.github.com>
2018-03-14 22:40:54 -0500

    Interest validations

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,4 +1,9 @@
 class Interest < ApplicationRecord
     has_many :contributors
     has_many :foundations
+
+    validates :name, presence: true
+    validates :name, format: { with: /\A[a-zA-Z]+\z/,message: "only allows letters" }
+    validates :description, length: {maximum: 500 }, allow_nil: true
+    
 end
