68db3139fd214895aaf787eca81867b6b3d729c9
Nicolás Campuzano <ncampuzano@users.noreply.github.com>
2018-03-31 20:30:03 -0500

    Enable cors

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,14 @@ module FundacionesApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    #Enable CORS
+    #
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource  '*', :headers => :any, :methods => [:get, :patch, :delete, :put, :post, :options]
+      end
+    end
   end
 end
