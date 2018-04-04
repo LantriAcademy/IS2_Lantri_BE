f9f0f0aaf4d4b6f805e158f3c696da8d51f7875f
Juan Manuel Alvarez Duque <jmalvarezd@unal.edu.co>
2018-04-04 05:55:51 +0000

    Implemented gems annotate and will_paginate

--- test/models/benefited_test.rb
+++ test/models/benefited_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: benefiteds
+#
+#  id            :integer          not null, primary key
+#  age           :integer
+#  preferences   :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  foundation_id :integer
+#  help_id       :integer
+#  picture_id    :integer
+#
+
 require 'test_helper'
 
 class BenefitedTest < ActiveSupport::TestCase
--- test/models/evidence_test.rb
+++ test/models/evidence_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: evidences
+#
+#  id          :integer          not null, primary key
+#  description :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  event_id    :integer
+#  picture_id  :integer
+#
+
 require 'test_helper'
 
 class EvidenceTest < ActiveSupport::TestCase
