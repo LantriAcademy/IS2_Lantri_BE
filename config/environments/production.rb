01004493b92ee46b60900766d6beeba6e89c7dda
Juan Manuel Alvarez Duque <jmalvarezd@unal.edu.co>
2018-04-05 20:15:22 +0000

    fixed production mailers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,21 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "fundaciones-api_#{Rails.env}"
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.perform_caching = false
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_setting = {
+    :address  => "smtp.gmail.com",
+    :port     => 587,
+    :domain => 'gmail.com',
+    :user_name => Rails.application.secrets.gmail_username,
+    :password => Rails.application.secrets.gmail_password,
+    :authentication => "plain",
+    :enable_starttls_auto => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
